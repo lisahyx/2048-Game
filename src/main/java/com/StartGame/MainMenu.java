5e656b961a5ba396f3f317a59871561d5a3d025e
Lisa Ho <lisahoyenxin@gmail.com>
2022-11-17 17:36:27 +0800

    Added game modes to choicebox

--- src/main/java/com/StartGame/MainMenu.java
+++ src/main/java/com/StartGame/MainMenu.java
@@ -1,4 +1,17 @@
 package com.StartGame;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
 public class MainMenu {
+    @FXML
+    private ChoiceBox gameMode;
+    ObservableList<String> modes = FXCollections.observableArrayList("4x4","5x5","6x6","Blackout");
+
+    public void initialize() {
+        gameMode.setValue("4x4"); // this statement shows default value
+        gameMode.setItems(modes);
+    }
 }
