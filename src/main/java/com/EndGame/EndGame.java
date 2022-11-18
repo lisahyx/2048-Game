fd757a8ac3ac787a36601bdb1606e36eb13134f9
Lisa Ho <lisahoyenxin@gmail.com>
2022-11-18 23:36:29 +0800

    Aligned elements to center of screen

--- src/main/java/com/EndGame/EndGame.java
+++ src/main/java/com/EndGame/EndGame.java
@@ -1,12 +1,17 @@
 package com.EndGame;
 
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -14,7 +19,9 @@ import javafx.stage.Stage;
 
 import java.util.Optional;
 
-
+/**
+ * A class for game over screen.
+ */
 public class EndGame {
     private static EndGame singleInstance = null;
     private EndGame(){
@@ -28,23 +35,34 @@ public class EndGame {
 
     public void endGameShow(Scene endGameScene, Group root, Stage primaryStage,long score){
         Text text = new Text("GAME OVER");
-        text.relocate(250,250);
         text.setFont(Font.font(80));
         root.getChildren().add(text);
 
-
         Text scoreText = new Text(score+"");
         scoreText.setFill(Color.BLACK);
-        scoreText.relocate(250,400);
         scoreText.setFont(Font.font(80));
         root.getChildren().add(scoreText);
 
         // quit button
         Button quitButton = new Button("QUIT");
-        quitButton.setPrefSize(100,30);
+        quitButton.setPrefSize(110,40);
         quitButton.setTextFill(Color.BLACK);
         root.getChildren().add(quitButton);
-        quitButton.relocate(250,550);
+
+        StackPane mainPane;
+
+        Stage window = primaryStage;
+
+        VBox vBox = new VBox(10);
+        vBox.setAlignment(Pos. CENTER);
+        vBox.prefWidthProperty().bind(window.widthProperty().multiply(0.90));
+
+        vBox.getChildren().addAll(text, scoreText, quitButton);
+
+        mainPane = new StackPane(vBox);
+        mainPane.setPadding(new Insets(30));
+        root.getChildren().add(mainPane);
+
         quitButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
