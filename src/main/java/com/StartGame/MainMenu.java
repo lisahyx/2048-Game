706cd4018b31540118c2693dcf76678735cc3b9f
Lisa Ho <lisahoyenxin@gmail.com>
2022-11-20 14:31:05 +0800

    Link start button to primary stage

--- src/main/java/com/StartGame/MainMenu.java
+++ src/main/java/com/StartGame/MainMenu.java
@@ -1,17 +1,123 @@
 package com.StartGame;
 
+import com.Game.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+import java.security.PrivateKey;
+import java.util.Scanner;
 
 public class MainMenu {
     @FXML
     private ChoiceBox gameMode;
+
+    @FXML
+    private Button startGame;
     ObservableList<String> modes = FXCollections.observableArrayList("4x4","5x5","6x6","Blackout");
 
     public void initialize() {
         gameMode.setValue("4x4"); // this statement shows default value
         gameMode.setItems(modes);
+
     }
-}
+
+    @FXML
+    EventHandler<ActionEvent> handleButtonAction = new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent actionEvent) {
+            Stage stage;
+            Parent root;
+
+            stage = (Stage) startGame.getScene().getWindow();
+            try {
+                root = FXMLLoader.load(getClass().getResource("/com/Game/game1.fxml"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+    };
+
+    // below is stage
+    static final int WIDTH = (int) Screen.getPrimary().getBounds().getWidth();
+    static final int HEIGHT = (int) Screen.getPrimary().getBounds().getHeight();
+    private Group gameRoot = new Group();
+    private Scene gameScene;
+
+    private static Scanner input= new Scanner(System.in);
+
+    public void setGameScene(Scene gameScene) {
+        this.gameScene = gameScene;
+    }
+
+    public void setGameRoot(Group gameRoot) {
+        this.gameRoot = gameRoot;
+    }
+
+    public void start () throws Exception{
+        Group menuRoot = new Group();
+        Scene menuScene = new Scene(menuRoot, WIDTH, HEIGHT);
+        Group accountRoot = new Group();
+        Scene accountScene = new Scene(accountRoot, WIDTH, HEIGHT, Color.rgb(150, 20, 100, 0.2));
+        Group getAccountRoot = new Group();
+        Scene getAccountScene = new Scene(getAccountRoot, WIDTH, HEIGHT, Color.rgb(200, 20, 100, 0.2));
+        Group endgameRoot = new Group();
+        Scene endGameScene = new Scene(endgameRoot, WIDTH, HEIGHT, Color.rgb(250, 20, 100, 0.2));
+        Group rankRoot = new Group();
+        Scene rankScene = new Scene(rankRoot, WIDTH, HEIGHT, Color.rgb(250, 50, 120, 0.3));
+        BackgroundFill background_fill = new BackgroundFill(Color.rgb(120, 100, 100), CornerRadii.EMPTY, Insets.EMPTY);
+        Background background = new Background(background_fill);
+
+
+        Rectangle backgroundOfMenu = new Rectangle(240, 120, Color.rgb(120, 120, 120, 0.2));
+        backgroundOfMenu.setX(WIDTH / 2 - 120);
+        backgroundOfMenu.setY(180);
+        menuRoot.getChildren().add(backgroundOfMenu);
+
+        Rectangle backgroundOfMenuForPlay = new Rectangle(240, 140, Color.rgb(120, 20, 100, 0.2));
+        backgroundOfMenuForPlay.setX(WIDTH / 2 - 120);
+        backgroundOfMenuForPlay.setY(180);
+        accountRoot.getChildren().add(backgroundOfMenuForPlay);
+
+        setGameRoot(gameRoot);
+
+        //gameScene
+        gameScene = new Scene(gameRoot, WIDTH, HEIGHT, Color.rgb(189, 177, 92));
+        setGameScene(gameScene);
+
+        Stage primaryStage = new Stage();
+
+        primaryStage.setScene(gameScene);
+        Game game = new Game();
+        game.game(gameScene, gameRoot, primaryStage, endGameScene, endgameRoot);
+
+        // Display the Stage
+        primaryStage.show();
+    }
+
+
+};
+
+
