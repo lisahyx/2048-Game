4f18c9a3cdc03f5399441c2c8ad4f2c60dfccd1c
Lisa Ho <lisahoyenxin@gmail.com>
2022-11-21 00:46:25 +0800

    Removed useless imports

--- src/main/java/com/StartGame/MainMenu.java
+++ src/main/java/com/StartGame/MainMenu.java
@@ -1,30 +1,15 @@
 package com.StartGame;
 
-import com.Game.*;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.lang.reflect.Method;
-import java.util.Scanner;
 
 public class MainMenu {
     @FXML
