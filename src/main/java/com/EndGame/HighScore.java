99f3cb8099f6f36a0fd5730fe607438bc524a759
Lisa Ho <lisahoyenxin@gmail.com>
2022-11-24 16:29:01 +0800

    Added structure for high score list

--- src/main/java/com/EndGame/HighScore.java
+++ src/main/java/com/EndGame/HighScore.java
@@ -1,4 +1,28 @@
 package com.EndGame;
 
+import com.Game.Game;
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class HighScore {
+    private static HighScore singleInstance = null;
+    public static HighScore getInstance() {
+        if(singleInstance == null)
+            singleInstance= new HighScore();
+        return singleInstance;
+    }
+
+    @FXML
+    private TextField userScores;
+
+    public void initialize() {
+        userScores.setText("username + score");
+    }
+
 }
