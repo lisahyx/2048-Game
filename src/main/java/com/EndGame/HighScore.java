90840bc814f564b70afe08239291d6a294644fef
Lisa Ho <lisahoyenxin@gmail.com>
2022-11-25 17:55:05 +0800

    Display high score list sorted by name

--- src/main/java/com/EndGame/HighScore.java
+++ src/main/java/com/EndGame/HighScore.java
@@ -1,32 +1,39 @@
 package com.EndGame;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class HighScore {
     private static HighScore singleInstance = null;
+
     public static HighScore getInstance() {
-        if(singleInstance == null)
-            singleInstance= new HighScore();
+        if (singleInstance == null)
+            singleInstance = new HighScore();
         return singleInstance;
     }
 
     @FXML
-    private TextField userScores;
+    private TextArea userScoreList;
 
-    public void initialize() {
-        userScores.setText(readList());
+    public void initialize() throws IOException {
+        userScoreList.setText(sortLines());
     }
 
+    // read last line of file
     private String readList() {
         try (BufferedReader reader = new BufferedReader(new FileReader(new File("highScoreList.txt")))) {
             String line = null;
             String nextLine;
+
             while ((nextLine = reader.readLine()) != null) {
                 line = nextLine;
             }
@@ -35,5 +42,14 @@ public class HighScore {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private String sortLines() throws IOException {
+        String sort = new String(Files.readAllBytes(Paths.get("highScoreList.txt")));
+        String[] wordy = sort.split("\n");
+        SortedSet<String> set = new TreeSet<String>();
+        set.addAll(Arrays.asList(wordy));
+        String sorted = set.stream().collect(Collectors.joining("\n"));
+        return sorted;
     }
 }
