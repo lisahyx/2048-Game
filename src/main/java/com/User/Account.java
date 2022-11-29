946258e6c2bf357fd477de1a29317e8393c64347
Lisa Ho <lisahoyenxin@gmail.com>
2022-11-29 15:07:58 +0800

    Added method to compare score for high score list & modified addUsername method

--- src/main/java/com/User/Account.java
+++ src/main/java/com/User/Account.java
@@ -1,54 +1,14 @@
 package com.User;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Objects;
-import java.util.Scanner;
 
-public class Account implements Comparable<Account> {
-    private long score = 0;
+public class Account {
+    private static long oldScore;
 
-    private String userName;
-    private static ArrayList<Account> accounts = new ArrayList<>();
-
-    @Override
-    public int compareTo(Account o) {
-        return Long.compare(o.getScore(), score);
-    }
-
-    public void addToScore(long score) {
-        this.score += score;
-    }
-
-    private long getScore() {
-        return score;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public static Account accountHaveBeenExist(String userName){
-        for(Account account : accounts){
-            if(account.getUserName().equals(userName)){
-                return account;
-            }
-        }
-        return null;
-    }
-
-    public static void makeNewAccount(String userName){
-        Account account = new Account();
-        accounts.add(account);
-    }
-
-    public static Account printAccount() {
-        System.out.print(accounts);
-        return null;
+    // get old score
+    public void getOldScore(String line) {
+        oldScore = Long.parseLong(line.substring(Integer.parseInt(String.valueOf(line.lastIndexOf(" ") + 1))));
     }
 
     public void addUsername(String username) throws IOException {
@@ -59,34 +19,22 @@ public class Account implements Comparable<Account> {
         BufferedReader reader = new BufferedReader(new FileReader(oldFile));
 
         String nextLine = reader.readLine();
-        String lastLine1 = null;
-        String lastLine2 = null;
+        String lastLine = null;
 
         FileWriter file_writer;
         file_writer = new FileWriter(newFile, true);
         BufferedWriter buffered_Writer = new BufferedWriter(file_writer);
 
-        if (nextLine == null){
-            lastLine2 = username.toUpperCase() + " ";
+        while (nextLine != null) {
+            if (Objects.equals(username.toUpperCase(), nextLine.substring(0, nextLine.indexOf(" ")))) {
+            getOldScore(nextLine);
+        } else {
+            buffered_Writer.write(nextLine + "\n");
         }
-        else {
-             while (nextLine != null) {
-                if (Objects.equals(username.toUpperCase(), nextLine.substring(0, nextLine.indexOf(" ")))) {
-                    lastLine1 = nextLine;
-                } else {
-                    buffered_Writer.write(nextLine + "\n");
-                    lastLine2 = username.toUpperCase() + " ";
-                }
-                nextLine = reader.readLine();
-            }
+            nextLine = reader.readLine();
         }
-
-        if(lastLine1 != null) {
-            buffered_Writer.write(lastLine1 + "\n");
-        } else if (lastLine2 != null) {
-            buffered_Writer.write(lastLine2);
-        }
-
+        lastLine = username.toUpperCase() + " ";
+        buffered_Writer.write(lastLine);
         buffered_Writer.flush();
         buffered_Writer.close();
         file_writer.close();
@@ -96,19 +44,18 @@ public class Account implements Comparable<Account> {
         newFile.renameTo(oldFile);
     }
 
-    // save score to file
-    public void addScore(long score){
-        Long line = score;
-
+    public void compareScore(long newScore) throws IOException {
         FileWriter file_writer;
-        try {
-            file_writer = new FileWriter("highScoreList.txt", true);
-            BufferedWriter buffered_Writer = new BufferedWriter(file_writer);
-            buffered_Writer.write(line+"\n");
-            buffered_Writer.flush();
-            buffered_Writer.close();
-        } catch (IOException e) {
-            System.out.println("Fail to save score" + e);
+        file_writer = new FileWriter("highScoreList.txt", true);
+        BufferedWriter buffered_Writer = new BufferedWriter(file_writer);
+
+        if(newScore > oldScore) {
+            buffered_Writer.write(newScore + "\n");
+        } else {
+            buffered_Writer.write(oldScore + "\n");
         }
+        buffered_Writer.flush();
+        buffered_Writer.close();
+        file_writer.close();
     }
 }
