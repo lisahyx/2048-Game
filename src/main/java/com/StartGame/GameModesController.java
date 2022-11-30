9b876b69ffb3f0b6322b24852ef3fc3bb935f974
Lisa Ho <lisahoyenxin@gmail.com>
2022-11-30 11:55:12 +0800

    Fixed minor warning bugs

--- src/main/java/com/StartGame/GameModesController.java
+++ src/main/java/com/StartGame/GameModesController.java
@@ -3,30 +3,19 @@ package com.StartGame;
 import com.Game.Game;
 import com.Game.GameScene;
 import com.User.Account;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 
 import java.io.*;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Scanner;
-
-import static com.Game.GameScene.getScore;
 
 /**
  * A controller for
@@ -71,8 +60,6 @@ public class GameModesController {
     private Group gameRoot = new Group();
     private Scene gameScene;
 
-    private static final Scanner input= new Scanner(System.in);
-
     public void setGameScene(Scene gameScene) {
         this.gameScene = gameScene;
     }
@@ -84,28 +71,8 @@ public class GameModesController {
     Account userScore = new Account();
 
     public void start () {
-        Group menuRoot = new Group();
-        Scene menuScene = new Scene(menuRoot, WIDTH, HEIGHT);
-        Group accountRoot = new Group();
-        Scene accountScene = new Scene(accountRoot, WIDTH, HEIGHT, Color.rgb(150, 20, 100, 0.2));
-        Group getAccountRoot = new Group();
-        Scene getAccountScene = new Scene(getAccountRoot, WIDTH, HEIGHT, Color.rgb(200, 20, 100, 0.2));
         Group endgameRoot = new Group();
         Scene endGameScene = new Scene(endgameRoot, WIDTH, HEIGHT, Color.rgb(250, 20, 100, 0.2));
-        Group rankRoot = new Group();
-        Scene rankScene = new Scene(rankRoot, WIDTH, HEIGHT, Color.rgb(250, 50, 120, 0.3));
-        BackgroundFill background_fill = new BackgroundFill(Color.rgb(120, 100, 100), CornerRadii.EMPTY, Insets.EMPTY);
-        Background background = new Background(background_fill);
-
-        Rectangle backgroundOfMenu = new Rectangle(240, 120, Color.rgb(120, 120, 120, 0.2));
-        backgroundOfMenu.setX(WIDTH / 2 - 120);
-        backgroundOfMenu.setY(180);
-        menuRoot.getChildren().add(backgroundOfMenu);
-
-        Rectangle backgroundOfMenuForPlay = new Rectangle(240, 140, Color.rgb(120, 20, 100, 0.2));
-        backgroundOfMenuForPlay.setX(WIDTH / 2 - 120);
-        backgroundOfMenuForPlay.setY(180);
-        accountRoot.getChildren().add(backgroundOfMenuForPlay);
 
         setGameRoot(gameRoot);
 
@@ -132,23 +99,6 @@ public class GameModesController {
         primaryStage.show();
         gameRoot.requestFocus(); // take away focus from quit button
 
-        // close window listener
-        primaryStage.setOnHiding(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent event) {
-                Platform.runLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            userScore.compareScore(getScore());
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                });
-            }
-        });
-
         // quit button
         Button quitButton = new Button("QUIT");
         quitButton.setPrefSize(85,35);
@@ -165,7 +115,7 @@ public class GameModesController {
             alert.setContentText("Are you sure?");
 
             Optional<ButtonType> result = alert.showAndWait();
-            if (result.get() == ButtonType.OK) {
+            if (result.orElse(null)  == ButtonType.OK) {
                 try {
                     userScore.compareScore(GameScene.getScore()); // write score to file
                 } catch (IOException e) {
